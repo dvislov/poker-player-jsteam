bc6ffb11cd55fc8d5239c2eb9f2c455434e36938
Alexander Andreev <alexander.andreev@restream.rt.ru>
2016-04-24 12:09:09 +0400

    sss

--- player.js
+++ player.js
@@ -1,20 +1,20 @@
-try {
-  const request = require('request');
-  const qs = require('querystring');
-
-  function call(bet, game_state) {
-    const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
-    bet(max_bet);
-  }
-
-  function raise(bet, game_state) {
-    const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
-    bet(max_bet + 100);
-  }
-
-  function fold(bet) {
-    bet(0);
-  }
+// try {
+//   const request = require('request');
+//   const qs = require('querystring');
+//
+//   function call(bet, game_state) {
+//     const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
+//     bet(max_bet);
+//   }
+//
+//   function raise(bet, game_state) {
+//     const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
+//     bet(max_bet + 100);
+//   }
+//
+//   function fold(bet) {
+//     bet(0);
+//   }
 
   module.exports = {
 
@@ -26,23 +26,23 @@ try {
       bet(max_bet);
     },
 
-    get_rank: function(cards, callback) {
-      const encoded = qs.escape(JSON.stringify(cards));
-
-      request("http://rainman.leanpoker.org/rank?cards=" + encoded, function(error, response, body) {
-        if(error) {
-          console.log(error);
-        }
-        callback(body);
-      });
-    },
+    // get_rank: function(cards, callback) {
+    //   const encoded = qs.escape(JSON.stringify(cards));
+    //
+    //   request("http://rainman.leanpoker.org/rank?cards=" + encoded, function(error, response, body) {
+    //     if(error) {
+    //       console.log(error);
+    //     }
+    //     callback(body);
+    //   });
+    // },
 
     showdown: function(game_state) {
 
     }
   };
-
-}
-catch(e){
-  console.log(e);
-}
+//
+// }
+// catch(e){
+//   console.log(e);
+// }

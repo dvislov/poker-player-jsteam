ece5c8283cab591d448b0414c13aa9f2cfe98e73
Alexander Andreev <alexander.andreev@restream.rt.ru>
2016-04-24 10:32:57 +0400

    initial

--- player.js
+++ player.js
@@ -4,7 +4,10 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-    bet(0);
+    const min = 0;
+    const max = 100;
+    const result = Math.random() * (max - min) + min;
+    bet(result);
   },
 
   showdown: function(game_state) {

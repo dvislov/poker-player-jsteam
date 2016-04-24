24f8eb8a76cf8cb855a06e0412f0eeeb3c939fab
Alexander Andreev <alexander.andreev@restream.rt.ru>
2016-04-24 12:25:32 +0400

    sss

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 try {
   const request = require('request');
   const qs = require('querystring');
-  const cards = require('./libs/cards');
+  const get_cards = require('./libs/cards');
 
   function call(bet, game_state) {
     const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
@@ -17,34 +17,35 @@ try {
     bet(0);
   }
 
+  function get_rank(cards, callback) {
+    const encoded = qs.escape(JSON.stringify(cards));
+
+    request("http://rainman.leanpoker.org/rank?cards=" + encoded, function(error, response, body) {
+      if(error) {
+        console.log(error);
+      }
+      callback(body);
+    });
+  }
+
+
+  function getDecision(rank, bet, state) {
+    if (rank > 0) {
+      raise(bet, state);
+    } else {
+      fold(bet, state);
+    }
+  }
+
   module.exports = {
 
     VERSION: 'Default JavaScript folding player',
 
-    bet_request: function(game_state, bet) {
-      const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
-
-      bet(max_bet);
-    },
-
-    get_rank: function(cards, callback) {
-      const encoded = qs.escape(JSON.stringify(cards));
-
-      request("http://rainman.leanpoker.org/rank?cards=" + encoded, function(error, response, body) {
-        if(error) {
-          console.log(error);
-        }
-        callback(body);
+    bet_request: function(game_state, bet) {      
+      var cards = get_cards(game_state);
+      get_rank(cards, function(state) {
+        getDecision(state.rank, bet, game_state);
       });
-    },
-
-    getDecision: function(rank, state) {
-      var bet = 0;
-      if (rank > 0) {
-        raise(bet, state);
-      } else {
-        fold(bet, state);
-      }
     },
 
     showdown: function(game_state) {

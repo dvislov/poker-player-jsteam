98a2c833fde24eae19b6f0f5fa4758aaf376b5c5
Alexander Andreev <alexander.andreev@restream.rt.ru>
2016-04-24 12:03:10 +0400

    ss

--- player.js
+++ player.js
@@ -1,44 +1,48 @@
-var getCards = require('./libs/cards');
+try {
+  const request = require('request');
+  const qs = require('querystring');
 
-const request = require('request');
-const qs = require('querystring');
-
-function call(bet, game_state) {
-  const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
-  bet(max_bet);
-}
-
-function raise(bet, game_state) {
-  const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
-  bet(max_bet + 100);
-}
-
-function fold(bet) {
-  bet(0);
-}
-
-module.exports = {
-
-  VERSION: 'Default JavaScript folding player',
-
-  bet_request: function(game_state, bet) {
+  function call(bet, game_state) {
     const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
-
     bet(max_bet);
-  },
-
-  get_rank: function(cards, callback) {
-    const encoded = qs.escape(JSON.stringify(cards));
-
-    request("http://rainman.leanpoker.org/rank?cards=" + encoded, function(error, response, body) {
-      if(error) {
-        console.log(error);
-      }
-      callback(body);
-    });
-  },
-
-  showdown: function(game_state) {
-
   }
-};
+
+  function raise(bet, game_state) {
+    const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
+    bet(max_bet + 100);
+  }
+
+  function fold(bet) {
+    bet(0);
+  }
+
+  module.exports = {
+
+    VERSION: 'Default JavaScript folding player',
+
+    bet_request: function(game_state, bet) {
+      const max_bet = Math.max.apply(null, game_state.players.map(p => p.bet));
+
+      bet(max_bet);
+    },
+
+    get_rank: function(cards, callback) {
+      const encoded = qs.escape(JSON.stringify(cards));
+
+      request("http://rainman.leanpoker.org/rank?cards=" + encoded, function(error, response, body) {
+        if(error) {
+          console.log(error);
+        }
+        callback(body);
+      });
+    },
+
+    showdown: function(game_state) {
+
+    }
+  };
+
+}
+catch(e){
+  console.log(e);
+}
